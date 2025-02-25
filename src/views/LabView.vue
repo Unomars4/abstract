f6a1f47cde46f47fc838af85fd1922d16dabe79d
Une Nyide <une.nyide@gmail.com>
2025-02-25 10:50:07 +0200

    add styling to lab view component

--- src/views/LabView.vue
+++ src/views/LabView.vue
@@ -2,5 +2,19 @@
 
 </script>
 <template>
-
+  <main>
+    <div class="canvas-container">
+      <canvas id="canvas" width="500px" height="500px" />
+    </div>
+  </main>
 </template>
+<style lang="css">
+.canvas-container {
+  margin: auto;
+  border: solid 1px var(--red-1);
+}
+
+#canvas {
+  border: solid 2px var(--dark-1);
+}
+</style>
