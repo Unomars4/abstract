b57133c66d3f8f46580390d677c650a0492c17d4
Une Nyide <une.nyide@gmail.com>
2025-03-13 22:06:56 +0200

    fix p5 canvas api connection

--- src/views/LabView.vue
+++ src/views/LabView.vue
@@ -1,20 +1,53 @@
-<script lang="ts">
+<script setup lang="ts">
+import p5 from "p5";
+import { onMounted, useTemplateRef } from "vue";
 
+const main = useTemplateRef<HTMLElement | undefined>("main");
+
+let sketch = function (p: p5) {
+  p.setup = function () {
+    p.createCanvas(500, 410);
+  }
+
+  p.draw = function () {
+    p.background(170, 0, 0);
+    p.fill(255);
+    p.rect(100, 100, 50, 50);
+  }
+}
+
+onMounted(() => {
+  if (main.value)
+    new p5(sketch, main.value)
+})
 </script>
+
+
+
 <template>
-  <main>
-    <canvas id="canvas" width="500px" height="500px" />
+  <main ref="main">
+    <h1>Lab</h1>
   </main>
 </template>
-<style lang="css">
+
+
+<style scoped lang="css">
+h1 {
+  font-weight: 900;
+  text-align: center;
+}
+
 main {
-  height: 100%;
+  height: 100vh;
+  width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  flex-wrap: wrap;
   align-content: center;
 }
 
-#canvas {
+.p5Canvas {
   border: solid 4px black;
   margin: auto;
 }
