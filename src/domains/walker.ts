048151adb49a29624154e11d54ed1f6b8b108743
Une Nyide <une.nyide@gmail.com>
2025-03-16 17:27:20 +0200

    add step method to wallker class and change availability for fields in walker class

--- src/domains/walker.ts
+++ src/domains/walker.ts
@@ -2,11 +2,11 @@
 import p5 from 'p5'
 
 class Walker {
-  readonly x: number
-  readonly y: number
+  x: number
+  y: number
   private readonly sketch: p5
 
-  constructor(width: number, height: number, sketch: p5) {
+  constructor(sketch: p5) {
     this.x = sketch.width / 2
     this.y = sketch.height / 2
     this.sketch = sketch
@@ -16,5 +16,20 @@ class Walker {
     const { x, y, sketch } = this
     sketch.stroke(125)
     sketch.point(x, y)
+  }
+
+  public step() {
+    let { sketch, x, y } = this
+    const choice = sketch.floor(sketch.random(4))
+
+    if (choice === 0) {
+      x++
+    } else if (choice === 1) {
+      x--
+    } else if (choice === 2) {
+      y++
+    } else {
+      y--
+    }
   }
 }
