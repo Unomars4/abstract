f3465ec80a568cc69b1c53102aed66ab3b55dcbe
Une Nyide <une.nyide@gmail.com>
2025-03-16 17:04:23 +0200

    add show method to walker class

--- src/domains/walker.ts
+++ src/domains/walker.ts
@@ -1,11 +1,20 @@
 //Walker Class
+import p5 from 'p5'
 
 class Walker {
   readonly x: number
   readonly y: number
+  private readonly sketch: p5
 
-  constructor(width: number, height: number) {
-    this.x = width / 2
-    this.y = height / 2
+  constructor(width: number, height: number, sketch: p5) {
+    this.x = sketch.width / 2
+    this.y = sketch.height / 2
+    this.sketch = sketch
+  }
+
+  public show() {
+    const { x, y, sketch } = this
+    sketch.stroke(125)
+    sketch.point(x, y)
   }
 }
