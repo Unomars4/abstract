d6d9bc3847eb17ac3f9c26a2152a5bb82f664c61
Une Nyide <une.nyide@gmail.com>
2025-03-16 16:51:45 +0200

    add constructor logic for walker class

--- src/domains/walker.ts
+++ src/domains/walker.ts
@@ -1,3 +1,11 @@
 //Walker Class
 
-class Walker {}
+class Walker {
+  readonly x: number
+  readonly y: number
+
+  constructor(width: number, height: number) {
+    this.x = width / 2
+    this.y = height / 2
+  }
+}
