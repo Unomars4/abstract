baf0a7f9e0468e4a1d08f1034e48cca0ba9f9721
Une Nyide <une.nyide@gmail.com>
2025-03-22 20:01:52 +0200

    import Footer into base App component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
 </script>
 
 <template>
   <Header />
   <main>
     <RouterView />
+    <Footer />
   </main>
 </template>
 
