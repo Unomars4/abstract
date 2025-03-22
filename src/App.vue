f0a2dced742f76f58a29041248f11e20d0705336
Une Nyide <une.nyide@gmail.com>
2025-03-22 20:00:44 +0200

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -7,28 +7,11 @@ import Header from "@/components/Header.vue"
   <Header />
   <main>
     <RouterView />
-    <footer>
-      <div class="footer-content">
-        <h3>Abstract</h3>
-        <p>Copyright © 2024 Une Nyide.</p>
-      </div>
-    </footer>
   </main>
 </template>
 
 <style scoped>
 main {
   height: 100%;
-}
-
-footer {
-  padding: 5px;
-  background: var(--gray-4);
-  border-top: 1px solid var(--gray-3);
-}
-
-.footer-content {
-  display: flex;
-  justify-content: space-between;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <div class="footer-content">
+      <h3>Abstract</h3>
+      <p>Copyright © 2024 Une Nyide.</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  padding: 5px;
+  background: var(--gray-4);
+  border-top: 1px solid var(--gray-3);
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
