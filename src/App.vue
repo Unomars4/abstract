9a3e5f49840bdd2113057250769b443b6afc31f4
Une Nyide <une.nyide@gmail.com>
2025-03-22 19:55:25 +0200

    import Header into base App component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Header from "@/components"
 </script>
 
 <template>
+  <Header />
   <main>
     <RouterView />
     <footer>
