2cb59d5d83910bb742cf0b1a93a8827cc43bf2f0
Une Nyide <une.nyide@gmail.com>
2025-03-22 19:53:51 +0200

    add Header component

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,6 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="header-content">
-      <a href="/">
-        <h1>Abstract</h1>
-      </a>
-      <nav>
-      </nav>
-    </div>
-  </header>
-
   <main>
     <RouterView />
     <footer>
@@ -25,21 +15,6 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header,
-.sidenav {
-  background: white;
-}
-
-header {
-  position: sticky;
-  top: 0px;
-}
-
-h1,
-h3 {
-  font-weight: 900;
-}
-
 main {
   height: 100%;
 }
@@ -48,13 +23,6 @@ footer {
   padding: 5px;
   background: var(--gray-4);
   border-top: 1px solid var(--gray-3);
-}
-
-header {
-  z-index: 2;
-  width: 100%;
-  border-bottom: solid var(--gray-3) 1px;
-  padding: 5px;
 }
 
 .footer-content {
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="header-content">
+      <a href="/">
+        <h1>Abstract</h1>
+      </a>
+      <nav>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="css">
+h1 {
+  font-weight: 900;
+}
+
+header {
+  position: sticky;
+  top: 0px;
+  background: white;
+  z-index: 2;
+  width: 100%;
+  border-bottom: solid var(--gray-3) 1px;
+  padding: 5px;
+}
+</style>
