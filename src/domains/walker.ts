10266134af0b341590f2ba105c8487f5c5df97ac
Une Nyide <une.nyide@gmail.com>
2025-05-02 13:43:56 +0200

    feat: increase walker stroke width

--- src/domains/walker.ts
+++ src/domains/walker.ts
@@ -1,33 +1,34 @@
 //Walker Class
-import p5 from 'p5'
+import p5 from 'p5';
 
 export default class Walker {
-  x: number
-  y: number
-  private readonly sketch: p5
+  x: number;
+  y: number;
+  private readonly sketch: p5;
 
   constructor(sketch: p5) {
-    this.x = sketch.width / 2
-    this.y = sketch.height / 2
-    this.sketch = sketch
+    this.x = sketch.width / 2;
+    this.y = sketch.height / 2;
+    this.sketch = sketch;
   }
 
   public show() {
-    this.sketch.stroke(0)
-    this.sketch.point(this.x, this.y)
+    this.sketch.strokeWeight(5);
+    this.sketch.stroke(0);
+    this.sketch.point(this.x, this.y);
   }
 
   public step() {
-    const choice = this.sketch.floor(this.sketch.random(4))
+    const choice = this.sketch.floor(this.sketch.random(4));
 
     if (choice === 0) {
-      this.x++
+      this.x++;
     } else if (choice === 1) {
-      this.x--
+      this.x--;
     } else if (choice === 2) {
-      this.y++
+      this.y++;
     } else {
-      this.y--
+      this.y--;
     }
   }
 }
