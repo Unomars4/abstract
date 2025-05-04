c383f8b5bef7b5a697f13cde6f18578f96c35d51
Une Nyide <une.nyide@gmail.com>
2025-05-04 23:16:28 +0200

    feat: add timeline component

---
+++ src/components/visualisations/timeline.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+type TimeLineProps<T> = {
+  data: Array<T>;
+  xAccessor: (item: T) => void;
+  yAcessor: (item: T) => void;
+  label: string;
+}
+const { data, xAccessor, yAcessor, label } = defineProps<TimeLineProps>()
+</script>
+
+<template>
+  <div class="timeline"></div>
+</template>
+
+<style scoped lang="css"></style>
