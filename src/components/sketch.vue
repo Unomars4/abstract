f154ac841dd52426120b417935b2b23bd2609ff1
Une Nyide <une.nyide@gmail.com>
2025-05-04 17:51:14 +0200

    fix: sketch container borders

--- src/components/sketch.vue
+++ src/components/sketch.vue
@@ -22,11 +22,12 @@ onMounted(() => {
 
 <style scoped lang="css">
 #container {
-  border: solid 1px var(--muted-foreground);
+  border: solid 1px hsl(var(--border));
   border-radius: var(--radius-xl);
   height: fit-content;
   width: fit-content;
   overflow: hidden;
   margin: 5px;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .1), 0 1px 2px -1px rgba(0, 0, 0, .1);
 }
 </style>
