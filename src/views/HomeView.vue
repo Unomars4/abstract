21eac82a15cf2d16bcb8eb096835e715b7a90f83
Une Nyide <une.nyide@gmail.com>
2025-05-04 13:13:31 +0200

    feat: add logo to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,23 @@
 </script>
 
 <template>
-
+  <div class="logo-container">
+    <h1>Abstract</h1>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-family: "Newsreader Variable";
+  font-size: 120px;
+  text-align: center;
+  font-weight: 800;
+}
+
+.logo-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
