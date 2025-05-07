932e8839bace3711b572600f4d2bac1ee492ad39
Une Nyide <une.nyide@gmail.com>
2025-05-08 00:02:50 +0200

    refactor: useChartDimensions composable

--- src/composables/useChartDimensions.ts
+++ src/composables/useChartDimensions.ts
@@ -1,4 +1,4 @@
-import { useTemplateRef, type ShallowRef, watchEffect, reactive } from 'vue';
+import { reactive } from 'vue';
 
 export type ChartDimensions = {
   width: number;
@@ -11,34 +11,24 @@ export type ChartDimensions = {
   boundedWidth: number;
 };
 
-export type ChartDimensionsRef = Readonly<ShallowRef<HTMLDivElement | null>>;
-
 // Regulates the chart boundaries, so it's positioned appropriately
 // Get the container's dimensions - width, height
 // uses default dimensions to size the graph space, but can also take in dimension options
 // Dimensions must be reactive and change according to screen size
-export default function useChartDimensions(refName: string): ChartDimensions {
-  const ref = useTemplateRef<HTMLElement>(refName),
-    dimensions: ChartDimensions = reactive({
-      width: window.innerWidth * 0.9,
-      height: 400,
-      marginTop: 15,
-      marginBottom: 40,
-      marginLeft: 60,
-      marginRight: 15,
-      boundedWidth: 0,
-      boundedHeight: 0,
-    });
-
-  watchEffect(() => {
-    if (!ref.value) return;
-    const { width, height } = ref.value.getBoundingClientRect();
-    dimensions.width = width;
-    dimensions.height = height;
-    dimensions.boundedHeight =
-      dimensions.height - dimensions.marginTop - dimensions.marginBottom;
-    dimensions.boundedWidth =
-      dimensions.width - dimensions.marginLeft - dimensions.marginRight;
+export default function useChartDimensions(
+  containerWidth: number,
+  containerHeight: number,
+  options,
+): ChartDimensions {
+  const dimensions: ChartDimensions = reactive({
+    width: containerWidth * 0.9,
+    height: containerHeight,
+    marginTop: 15,
+    marginBottom: 40,
+    marginLeft: 60,
+    marginRight: 15,
+    boundedWidth: 0,
+    boundedHeight: 0,
   });
 
   return dimensions;
