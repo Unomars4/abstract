c867342ff57c7c280451fdf97b18a00beab75b37
Une Nyide <une.nyide@gmail.com>
2025-05-08 18:03:51 +0200

    refactor: chart wrapper

--- src/components/visualisations/chart.vue
+++ src/components/visualisations/chart.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import useChartDimensions from '@/composables/useChartDimensions';
+
 const { height, width } = defineProps<{ width: number; height: number }>()
+const dimensions = useChartDimensions(width, height)
 </script>
 
 <template>
   <svg :width="width" :height="height" class="chart">
-    <g :transform="">
+    <g :transform="`translate(${dimensions.state.marginLeft}, ${dimensions.state.marginTop})`">
       <slot />
     </g>
   </svg>
