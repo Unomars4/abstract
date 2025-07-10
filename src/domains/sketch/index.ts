bb66645b5e1fea96cf8b79235ecf37ff950b4481
Une Nyide <une.nyide@gmail.com>
2025-07-10 15:46:35 +0200

    chore: remove sketch class domains

--- src/domains/sketch/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SketchDetails } from '@/types';
-import type p5 from 'p5';
-
-export class Sketch {
-  constructor({ title }: Omit<SketchDetails, 'id' | 'isPlaying'>) {}
-
-  public sketch(p: p5) {}
-}
